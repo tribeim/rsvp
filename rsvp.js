5474a859e9caef42bbbdd795c4a4423fb2aad18e
Sebastiaan Deckers <sebdeckers83@gmail.com>
2011-11-18 01:29:49 +0800

    Entrance animation for the envelope and invitation
    Added animate.css library

--- rsvp.js
+++ rsvp.js
@@ -1,3 +1,15 @@
 /**
  RSVP for Tribe.im
  **/
+$(function () {
+	$("#invitation")
+		.css({visibility: "hidden"});
+	$("#rsvp")
+		.css({visibility: "visible"})
+		.addClass("animated bounceIn");
+	setTimeout(function() {
+		$("#invitation")
+			.css({visibility: "visible"})
+			.addClass("animated flipInX");
+	}, 800);
+});

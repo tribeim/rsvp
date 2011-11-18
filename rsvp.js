e13341264c6ad1e843e725d602f617d01dace19b
Sebastiaan Deckers <cbas@pandion.be>
2011-11-18 21:33:35 +0800

    Added delay to animation start

--- rsvp.js
+++ rsvp.js
@@ -2,14 +2,16 @@
  RSVP for Tribe.im
  **/
 $(window).bind("load", function () {
-	$("#rsvp")
-		.css({visibility: "hidden"});
-	$("#invitation")
-		.css({visibility: "visible"})
-		.addClass("animated bounceIn");
 	setTimeout(function () {
 		$("#rsvp")
+			.css({visibility: "hidden"});
+		$("#invitation")
 			.css({visibility: "visible"})
-			.addClass("animated flipInX");
-	}, 800);
+			.addClass("animated bounceIn");
+		setTimeout(function () {
+			$("#rsvp")
+				.css({visibility: "visible"})
+				.addClass("animated flipInX");
+		}, 800);
+	}, 400);
 });
